1cf5a617b611564e6f1a759df75b414998d5ebbd
Gigi Campo <gigi.campo@gmail.com>
2016-04-01 10:35:19 -0400

    debug heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   before_validation :ensure_session_token
 
   # has_many: favorites
-  has_attached_file :image, default_url: "assets/images/default.jpg"
+  has_attached_file :image, default_url: asset_url("images/default.jpg")
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   def self.find_by_credentials(email, password)
