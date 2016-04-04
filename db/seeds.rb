8c13f7b78cd0b99d06a2424f653d5704f9bf8e7e
Gigi Campo <gigi.campo@gmail.com>
2016-04-04 16:58:56 -0400

    begin building guest-login-button

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-User.destroy_all
 
-User.create!(name: "Gigi", zipcode: "10014", email: "gigi.campo@gmail.com",
-search_sex: "F", search_age: 3, search_size: "small", about_me: "I have a pup!",
-about_life: "snuggles and playing fetch", password: "lunapup",
-ideal_dog: "likes to sleep in but is still game to go for a jog. Also loves carrots.")
+
+gigi = User.create!(name: "Gigi", zipcode: "10014", email: "gigi.campo@gmail.com",
+password_digest: "$2a$10$OprmvzZSU/TLZ/fxq/EbkegN2H0Ub.FfMK3tOMY6zIZ5s5WvpjTJe",
+search_sex: "F", search_age: "Young", search_size: "Small",
+about_me: "I am an App Academy student with tons of free time to spend playing with a puppy.",
+about_life: "My dog will be showered with treats constantly and should expect 20-100 minutes
+of belly rubs per day.", ideal_dog: "likes to sleep in, but is still game to go out for a jog in the morning. Also, loves carrots.")
